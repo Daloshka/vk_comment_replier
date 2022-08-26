01e5cd9d24c6ca0bfac43508c64f05d4bda426b8
Lerrrkk <36123350+Daloshka@users.noreply.github.com>
2022-08-26 18:42:47 +0800

    Add files via upload

--- info.py
+++ info.py
@@ -3,8 +3,11 @@ from random import randint
 # Ссылка для получения токена VK https://oauth.vk.com/authorize?client_id=8129750&scope=wall,groups,offline&redirect_uri=https://oauth.vk.com/blank.html&display=page&response_type=token&revoke=1
 # Скопировать между [access_token=]   df32b9dc3fd95ff6f8c8e3377b1b313f8daa84a3c39fc95803ea6b03f3421eda1425bcacc29f390f9cd40   [&expires_in]  (FAKE TOKEN)
 
-
 token_vk = ""
+
+# Ссылка на получение API ключа для обхода капчи https://clck.ru/uzMVY
+
+ANTICAPTCHA_KEY = ""
 
 # Сюда нужно встаить id группы от лица, которой будут отправляться сообщение (токен вк должен иметь доступ админа в этой группе)
 # Можно получить тут https://regvk.com/id/
@@ -17,6 +20,8 @@ delay_for_message = randint(30, 50)
 # text =  ["Текст1", "Текст2"]   добавляете разный текст через запятую в подобном формате  , "Текст3"
 text = ["Го к нам, давай к нам"]
 
-
 comments_under_each_post = 5
 posts_each_group = 5
+
+
+
--- main.py
+++ main.py
@@ -1,13 +1,53 @@
-import vk_api
-import random
-import info  # bot_token
-from random import randint
 import datetime
+import vk_api
+from vk_api import Captcha
+import random
+from random import randint
+import info  # bot_token
+from info import ANTICAPTCHA_KEY
 import time
+from time import sleep
+import re
+import onnxruntime as rt
+import requests
 
-captcha_sid = None
-captcha_key = None
-captcha_url = 'Ссылка пуста'
+
+session = vk_api.VkApi(token= info.token_vk, captcha_handler=captcha_handler)
+
+def get_responce_captcha(_id):
+    print('Ожидание выполнения капчи.')
+    isGet = False
+    text = None
+    while (isGet == False):
+        print('Попытка получить капчу')
+        r = requests.get(f'https://rucaptcha.com/res.php?key={ANTICAPTCHA_KEY}&action=get&id={_id}')
+        text = r.text.split('|')[-1]
+        if (text != 'ERROR_WRONG_CAPTCHA_ID' and text != 'CAPCHA_NOT_READY' and r.status_code == 200):
+            isGet = True
+    print(f"Капча получена: {text}\n5 секунд до ввода")
+    time.sleep(5)
+    return text
+
+def captcha_handler(captcha: Captcha):
+    """
+    Хендлер для обработки капчи из VK
+    """
+    print('Обнаружена капча!')
+    captcha_url = captcha.get_url()
+    base_64_link = base64.b64encode(requests.get(captcha_url).content).decode("utf-8")
+    data = {
+        'key': ANTICAPTCHA_KEY,
+        'method': 'base64',
+        'body': base_64_link,
+        'soft_id': 788289
+    }
+    r = requests.post('http://rucaptcha.com/in.php', data=data)
+    print(r.status_code)
+    _id = r.text.split('|')[-1]
+    print(_id)
+    key = get_responce_captcha(_id)
+    #key = input("\n\n[!] Чтобы продолжить, введи сюда капчу с картинки {0}:\n> ".format(captcha.get_url())).strip()
+    return captcha.try_again(key)
 
 # Чтение ссылок из текстового документа
 def read_lines_from_txt():
@@ -17,28 +57,22 @@ def read_lines_from_txt():
         lines = [line.rstrip() for line in lines]
         for line in lines:
             list_links.append(line)
-        # print(list_links)
         return list_links
 
 # return negative ID for group from URL
 def get_id_by_link(url):
-    session = vk_api.VkApi(token= info.token_vk)
     short_url = url.split('/')[-1]
-    #print(short_url)
     group_id = session.method('groups.getById', {'group_ids':short_url})[0]['id']
     group_id *= -1
     print(group_id)
     return group_id
 
 def get_owner_id_and_post_id_last_post(id):
-    session = vk_api.VkApi(token= info.token_vk)
     owner_id = session.method('wall.get', {'owner_id':id,'count':1})['items'][0]['owner_id']
     post_id = session.method('wall.get', {'owner_id':id,'count':1})['items'][0]['id']
-    #print(owner_id, post_id)
     return owner_id, post_id
 
 def get_owner_id_and_post_id_five_posts(id):
-    session = vk_api.VkApi(token= info.token_vk)
     # сколько постов комментить в каждой группе
     list_post_id = []
     owner_id = id
@@ -47,18 +81,15 @@ def get_owner_id_and_post_id_five_posts(id):
             list_post_id.append(session.method('wall.get', {'owner_id':owner_id})['items'][i]['id'])
         except:
             print(f"Комментарии под постом закончились")
-    #print(owner_id, post_id)
     return owner_id, list_post_id
 
 def leave_comment_under_post(owner_id, post_id):
     message = info.text[randint(0, len(info.text)-1)]
-    session = vk_api.VkApi(token= info.token_vk)
     session.method('wall.createComment', {'owner_id':owner_id,'post_id':post_id, 'message':message, 'from_group':info.your_group_id,'random_id' : random.randint(1000, 99999)})
     print(f"Был оставлен комментарий")
 
 # получаем id 5 комментариев под постом
 def get_comments_id_under_post(post_owner_id, post_id):
-    session = vk_api.VkApi(token= info.token_vk)
     comment_ids = []
     comments = session.method('wall.getComments', {'owner_id':post_owner_id,'post_id':post_id,'count':info.comments_under_each_post,'random_id' : random.randint(1000, 99999)})['items']
     for comment in comments:
@@ -75,30 +106,17 @@ def get_comments_id_under_post(post_owner_id, post_id):
     return comment_ids
 
 def create_comment(owner_id, post_id, reply_to_comment, from_group = 0):
-    global captcha_sid
-    global captcha_key
-    try:
-        if (captcha_sid is not None):
-            session = vk_api.VkApi(token= info.token_vk)
-            message = info.text[randint(0, len(info.text)-1)]
-            print(owner_id, post_id, reply_to_comment, from_group)
-            session.method('wall.createComment', {'owner_id':owner_id,'post_id':post_id, 'reply_to_comment':reply_to_comment, 'message':message, 'from_group':from_group, 'captcha_sid':captcha_sid, 'captcha_key':input(f'Введите капчу указаную в ссылке: {captcha_url}\n')})
-            print('Сообщение прошло капчу')
-            captcha_key = None
-            captcha_sid = None
-        else:
-            session = vk_api.VkApi(token= info.token_vk)
-            message = info.text[randint(0, len(info.text)-1)]
-            print(owner_id, post_id, reply_to_comment, from_group)
-            session.method('wall.createComment', {'owner_id':owner_id,'post_id':post_id, 'reply_to_comment':reply_to_comment, 'message':message, 'from_group':from_group})
-    except vk_api.exceptions.Captcha as captcha:
-        captcha_sid = captcha.sid
-        #print(captcha.sid) # Получение sid
-        captcha_url = captcha.get_url()
-        print(captcha.get_url()) # Получить ссылку на изображение капчи
-        #print(captcha.get_image()) # Получить изображение капчи (jpg)
-    
-
+    if (captcha_sid is not None):
+        message = info.text[randint(0, len(info.text)-1)]
+        #print(owner_id, post_id, reply_to_comment, from_group)
+        session.method('wall.createComment', {'owner_id':owner_id,'post_id':post_id, 'reply_to_comment':reply_to_comment, 'message':message, 'from_group':from_group, 'captcha_sid':captcha_sid, 'captcha_key':input(f'Введите капчу указаную в ссылке: {captcha_url}\n')})
+        print('Сообщение прошло капчу')
+        captcha_key = None
+        captcha_sid = None
+    else:
+        message = info.text[randint(0, len(info.text)-1)]
+        print(owner_id, post_id, reply_to_comment, from_group)
+        session.method('wall.createComment', {'owner_id':owner_id,'post_id':post_id, 'reply_to_comment':reply_to_comment, 'message':message, 'from_group':from_group})
 
 def main():
     list_links = read_lines_from_txt()
@@ -109,39 +127,18 @@ def main():
             # получаем id на последний пост
             post_owner_id, list_post_id = get_owner_id_and_post_id_five_posts(group_id)
             for i in range(len(list_post_id)):
-                try:
-                    post_id = list_post_id[i]
-                    # пишем коммент под последнии 5 комментов от лица вашего сообщества пользователям под их comment id 
-                    comment_ids = get_comments_id_under_post(post_owner_id, post_id)
-                    print(f"Comment_ids {comment_ids}")
-                    for reply_to_comment in comment_ids:
-                        #print(reply_to_comment)
-                        #print(type(info.your_group_id), info.your_group_id)
-                        create_comment(post_owner_id, post_id, reply_to_comment, info.your_group_id)
-                        print(f"Ответ на комментарий {post_owner_id} отправлен")
-                        time.sleep(info.delay_for_message)
-                except Exception as ex:
-                    print(f"!!! ERROR  !!!!")
-                    print(post_owner_id, list_post_id[i], ex)
-                    print(f"Error in list_post_id {ex}")
+                post_id = list_post_id[i]
+                # пишем коммент под последнии 5 комментов от лица вашего сообщества пользователям под их comment id 
+                comment_ids = get_comments_id_under_post(post_owner_id, post_id)
+                print(f"Comment_ids {comment_ids}")
+                for reply_to_comment in comment_ids:
+                    create_comment(post_owner_id, post_id, reply_to_comment, info.your_group_id)
+                    print(f"Ответ на комментарий {post_owner_id} отправлен")
+                    time.sleep(info.delay_for_message)
         except Exception as ex:
             print(f"Error in list_links {ex}")
 
-# print(get_owner_id_and_post_id_last_post(-213025701))
-# owner_id, post_id = get_owner_id_and_post_id_last_post(-213025701)
-# leave_comment_under_post(owner_id, post_id)
 
 main()
 time.sleep(20)
-
-
-#post_id_id, post_id = get_owner_id_and_post_id_last_post(-213025701)
-
-#print(get_owner_id_and_post_id_five_posts(-213025701))
-
-#print(get_comments_id_under_post(-213025701, 2))
-
-# session = vk_api.VkApi(token= info.token_vk)
-# session.method('wall.createComment', {'owner_id' :user_id, 'post_id':,'from_group':0,'message':info.text[randint(0, len(info.text)-1)], 'random_id' : random.randint(1000, 99999)})
-
 
